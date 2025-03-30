b79ee1a96984a8e0cba2ad4395d888247094f846
TobiasKlanert <tobias.klanert@t-online.de>
2025-03-30 17:10:47 +0200

    randomize cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,20 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
-
   pickCardAnimation: boolean = false;
+  currentCard: string = '';
+  game: Game;
 
-  constructor() {}
+  constructor() {
+    this.game = new Game();
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
 }
