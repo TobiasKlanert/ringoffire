e3ffd76c0ca7a7b68bc73c3600d99c9583b83938
TobiasKlanert <tobias.klanert@t-online.de>
2025-03-31 18:49:59 +0200

    added plus button

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerComponent } from "../player/player.component";
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
